ff23da3bd81753f46bf714e50c34726dd75ad84f
ElvisCayo <parapeggoelvis@gmail.com>
2024-09-30 10:26:34 -0300

    Add files via upload

---
+++ scipt.js
@@ -0,0 +1,18 @@
+class muffin{
+    constructor (sabor, tamaño, cantidad){
+        this.sabor = sabor;
+        this.tamaño = tamaño;
+        this.cantidad = cantidad;
+    }
+}
+
+const deliciaDeChocolate = new muffin("Chocolate", "Mediano", "12 unidades");
+const vacaLechera = new muffin("4 leches", "Mediano", "12 unidades");
+const mantecados = new muffin();
+const cremaConAlmendras = new muffin();
+const saborManzana = new muffin();
+const saborNaranja = new muffin();
+const saborDulceDeLeche = new muffin();
+
+
+console(deliciaDeChocolate)
